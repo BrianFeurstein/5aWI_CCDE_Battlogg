54063a8ff71a4bb2d6f61d616f25238ed1c413cc
BrianFeurstein <79904813+BrianFeurstein@users.noreply.github.com>
2022-10-06 19:58:15 +0200

    vollständiges Image-File

---
+++ server.js
@@ -0,0 +1,29 @@
+
+
+'use strict';
+
+let obj = {};
+console.log("starting......")
+
+const express = require('express');
+const { lookup } = require('geoip-lite');
+const router = express.Router();
+
+// App
+const app = express();
+var os = require('os');
+
+
+
+router.get('/info', (req,res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const info = {hostname:os.hostname(), platform:os.platform(),ip:ip}
+    res.send(JSON.stringify(info)) // ip address of the user
+});
+  
+  app.use('/', router);
+  app.listen(8080);
+
+  
+
+
